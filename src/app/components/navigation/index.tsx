7f3eb9fc09262fde648ada57477445431b75fe3f
Niraj Sapkota <n.sapkota@outlook.com.au>
2024-12-28 15:22:54 +1100

    Add underline animation to navigation elements on hover

--- src/app/components/navigation/index.tsx
+++ src/app/components/navigation/index.tsx
@@ -21,14 +21,15 @@ const links = [
 
 function Navigation() {
     return (
-        <nav className="hidden md:flex relative">
+        <nav className="hidden md:flex">
             {links.map((link) => (
                 <Link
                     key={link.title}
                     href={link.link}
-                    className="ml-6 text-gray-800 hover:text-gray-600 relative inline-block transition-colors underline-animation"
+                    className="ml-6 text-gray-800 hover:text-gray-600 relative inline-block group"
                 >
                     {link.title}
+                    <span className="absolute w-full max-w-0 h-[2px] right-0 -bottom-1 bg-black group-hover:left-0 group-hover:max-w-full transition-all duration-300"></span>
                 </Link>
             ))}
         </nav>
